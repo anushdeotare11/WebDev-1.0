22ff53ab4e703031c0f6719091289e14c8330c5a
Anush Deotare <anushdeotare07@gmail.com>
2026-03-08 13:46:17 +0530

    add the answer for the anagram question

--- 01-js/easy/anagram.js
+++ 01-js/easy/anagram.js
@@ -5,6 +5,29 @@
 */
 
 function isAnagram(str1, str2) {
+  str1=str1.toLowerCase().replace(/\s/g,"")
+  str2=str2.toLowerCase().replace(/\s/g,"")
+  let ArrayStr1 = [], ArrayStr2 = [];
+  if (str1.length != str2.length) {
+    return false;
+  }
+  else {
+    for (let i = 0; i < str1.length; i++) {
+      ArrayStr1.push(str1[i]);
+    }
+    for (let j = 0; j < str2.length; j++) {
+      ArrayStr2.push(str2[j]);
+    }
+    ArrayStr1.sort();
+    ArrayStr2.sort();
+    for (let k = 0; k < str1.length; k++) {
+      if (ArrayStr1[k] != ArrayStr2[k]) {
+        return false;
+      }
+    }
+    return true;
+
+  }
 
 }
 
